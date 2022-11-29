b4bd5527467891727ed33947195403ffda7f88f9
Hiroki Kajiya <kajiya.hiroki@gmail.com>
2022-11-29 18:31:47 +0900

    footer を作成した

---
+++ assets/shared/js/footer.js
@@ -0,0 +1,7 @@
+const footerContainer = document.querySelector('#js-Footer_Container');
+
+fetch('/assets/shared/templates/footer.html')
+  .then((response) => response.text())
+  .then((html) => {
+    footerContainer.insertAdjacentHTML('afterend', html);
+  });
