8359d08d7617f03633ce8b0fcaf0af8c1231e3ea
あんじ <Fuku.ankofuumi@gmail.com>
2022-12-16 16:27:19 +0900

    no message

--- assets/shared/js/footer.js
+++ assets/shared/js/footer.js
@@ -1,8 +1,12 @@
 const footerContainer = document.querySelector('#js-Footer_Container');
-const basaURL = location.host === 'anna-da.github.io' ? `${location.protocol}//${location.host}/Flowers-and-paintings/` : '';
+const basaURL = location.host === 'anna-da.github.io' ? `${location.protocol}//${location.host}/Flowers-and-paintings/` : '/';
 
-fetch(basaURL+'assets/shared/templates/footer.html')
+fetch(basaURL + 'assets/shared/templates/footer.html')
   .then((response) => response.text())
   .then((html) => {
     footerContainer.insertAdjacentHTML('afterend', html);
+
+    const footerHeaderHeadingImg = document.querySelector('#js-sw-FooterHeader_HeadingImg');
+
+    footerHeaderHeadingImg.src = basaURL + 'assets/shared/images/footer-icon-0.png';
   });
