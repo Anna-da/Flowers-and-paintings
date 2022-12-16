43d2511f4973859afa6c42c230f34409a9d49954
あんじ <Fuku.ankofuumi@gmail.com>
2022-12-16 15:07:26 +0900

    リンクがんばれ

--- assets/shared/js/footer.js
+++ assets/shared/js/footer.js
@@ -1,6 +1,6 @@
 const footerContainer = document.querySelector('#js-Footer_Container');
 
-fetch('/assets/shared/templates/footer.html')
+fetch('../templates/footer.html')
   .then((response) => response.text())
   .then((html) => {
     footerContainer.insertAdjacentHTML('afterend', html);
