779ab2b02d373ccd6c60a6f130e47dfc7148e974
あんじ <Fuku.ankofuumi@gmail.com>
2022-12-16 15:50:36 +0900

    フッター改

--- assets/shared/js/footer.js
+++ assets/shared/js/footer.js
@@ -1,6 +1,7 @@
 const footerContainer = document.querySelector('#js-Footer_Container');
+const basaURL = location.host === 'anna-da.github.io' ? `${location.protocol}://${location.host}/Flowers-and-paintings/` : '';
 
-fetch('assets/shared/templates/footer.html')
+fetch(basaURL+'assets/shared/templates/footer.html')
   .then((response) => response.text())
   .then((html) => {
     footerContainer.insertAdjacentHTML('afterend', html);
