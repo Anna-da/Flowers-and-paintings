f127c4b200b387edef0a68ceaebea7785d34590f
Hiroki Kajiya <kajiya.hiroki@gmail.com>
2023-01-24 18:09:55 +0900

    グローバルヘッダーの外部ファイル化

--- assets/shared/js/footer.js
+++ assets/shared/js/footer.js
@@ -1,12 +1,21 @@
-const footerContainer = document.querySelector('#js-Footer_Container');
-const basaURL = location.host === 'anna-da.github.io' ? `${location.protocol}//${location.host}/Flowers-and-paintings/` : '/';
+(function () {
+  const footerContainer = document.querySelector('#js-Footer_Container');
+  const basaURL = location.host === 'anna-da.github.io' ? `${location.protocol}//${location.host}/Flowers-and-paintings` : '';
 
-fetch(basaURL + 'assets/shared/templates/footer.html')
-  .then((response) => response.text())
-  .then((html) => {
-    footerContainer.insertAdjacentHTML('afterend', html);
+  fetch(basaURL + 'assets/shared/templates/footer.html')
+    .then((response) => response.text())
+    .then((html) => {
+      footerContainer.insertAdjacentHTML('afterend', html);
 
-    const footerHeaderHeadingImg = document.querySelector('#js-sw-FooterHeader_HeadingImg');
+      const root = document.querySelector('#js-sw-Footer');
+      const anchorList = root.querySelectorAll('a[href]');
+      const footerHeaderHeadingImg = document.querySelector('#js-sw-FooterHeader_HeadingImg');
 
-    footerHeaderHeadingImg.src = basaURL + 'assets/shared/images/footer-icon-0.png';
-  });
+      anchorList.forEach((anchor) => {
+        const href = anchor.getAttribute('href');
+        anchor.href = basaURL + href;
+      });
+
+      footerHeaderHeadingImg.src = basaURL + '/assets/shared/images/footer-icon-0.png';
+    });
+})();
---
+++ assets/shared/js/header.js
@@ -0,0 +1,23 @@
+(function () {
+  const headerContainer = document.querySelector('#js-Header_Container');
+  const basaURL = location.host === 'anna-da.github.io' ? `${location.protocol}//${location.host}/Flowers-and-paintings` : '';
+
+  fetch(basaURL + 'assets/shared/templates/header.html')
+    .then((response) => response.text())
+    .then((html) => {
+      headerContainer.insertAdjacentHTML('afterend', html);
+
+      const root = document.querySelector('#js-sw-Header');
+      const anchorList = root.querySelectorAll('a[href]');
+      const headerLogoImgList = document.querySelectorAll('.js-sw-Header_LogoImg');
+
+      anchorList.forEach(anchor => {
+        const href = anchor.getAttribute('href');
+        anchor.href = basaURL + href;
+      });
+
+      headerLogoImgList.forEach(headerLogoImg => {
+        headerLogoImg.src = basaURL + '/assets/shared/images/header-logo-0.png';
+      });
+    });
+})();
